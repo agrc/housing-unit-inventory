56d139669e10d4ae2efd83bd10c882a84d894810
Jake Adams <jdadams@utah.gov>
2022-03-22 19:57:15 -0600

    chore: profile, find slow spots

--- src/housing_unit_inventory/main.py
+++ src/housing_unit_inventory/main.py
@@ -1,11 +1,29 @@
 import logging
+from timeit import default_timer
 
 from housing_unit_inventory import county_scripts
 
 
 def process():
+
+    start = default_timer()
     logging.basicConfig(level=logging.DEBUG)
+
+    # import cProfile
+    # import pstats
+
+    # pr = cProfile.Profile()
+    # pr.enable()
+
     county_scripts.davis_county()
+
+    # pr.disable()
+    # with open(r'c:\temp\davis_profile.txt', 'w+') as f:
+    #     ps = pstats.Stats(pr, stream=f)
+    #     ps.strip_dirs().sort_stats(-1).print_stats()
+
+    end = default_timer()
+    logging.debug(f'Total time: {end - start}')
 
 
 if __name__ == '__main__':
