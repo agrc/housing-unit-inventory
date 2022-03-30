8eef8378e366d1bcecfd3d84141030d828891355
Jake Adams <jdadams@utah.gov>
2022-03-30 09:13:10 -0600

    wip: attribute dissolving

--- src/housing_unit_inventory/dissolve.py
+++ src/housing_unit_inventory/dissolve.py
@@ -1,4 +1,5 @@
 import logging
+import warnings
 from collections import defaultdict
 
 import pandas as pd
@@ -41,25 +42,77 @@ def _dissolve_geometries(duplicates_df, dissolve_field):
     return dissolved_geometries_df
 
 
-def _dissolve_attributes(duplicates_df, dissolve_field, fields_map):
+def _dissolve_attributes(duplicates_df, dissolve_field, fields_map, sum_duplicate_test_fields):
 
-    #: Count, First, sum, max
+    dissolved_attributes_df = pd.DataFrame()
 
+    groupby = duplicates_df.groupby(dissolve_field)
     ops_and_fields = _group_common_field_ops(fields_map)
-    for op, fields in ops_and_fields.items():
-        if op == 'count':
+    for op_name, op_fields in ops_and_fields.items():
+        if op_name not in ['count', 'first', 'sum', 'max']:
+            warnings.warn(f'Attribute operation "{op_name}" not supported, skipping...')
+            continue
+        if dissolve_field in op_fields and op_name != 'count':
+            warnings.warn(
+                f'Dissolve field "{dissolve_field}" should only use "count" operation; result likely nonsensical'
+            )
+        if op_name == 'count':
+            op_df = groupby[op_fields].count()
+        if op_name == 'first':
+            op_df = groupby[op_fields].first()
+        if op_name == 'sum':
+            op_df = groupby.agg(_smart_groupby_sum, sum_fields=op_fields,
+                                test_fields=sum_duplicate_test_fields)[op_fields]
+        if op_name == 'max':
+            op_df = groupby[op_fields].max()
+
+        #: Because of the groupby, the row index should be the same for each op, so we concat along the columns
+        dissolved_attributes_df = pd.concat([dissolved_attributes_df, op_df], axis='columns')
+
+    #: Rename dissolve field with name_op if it's in the field map (prevents having a column the same as the index)
+    if dissolve_field in fields_map:
+        dissolved_attributes_df.rename(
+            columns={dissolve_field: f'{dissolve_field}_{fields_map[dissolve_field]}'}, inplace=True
+        )
+
+    return dissolved_attributes_df  #.reset_index()
+
+
+def _smart_groupby_sum(group_df, sum_fields, test_fields):
+    """Return sum of each column only if all values in test_fields are different; return first row if the same
+
+    Args:
+        group_df (pd.DataFrame): Dataframe, potentially a groupby group dataframe
+        sum_fields (list[str]): Column names to sum/return
+        test_fields (list[str]): Column names to use for evaluating whether rows are duplicates
+
+    Returns:
+        pd.Series: Either the first duplicated row or the sum of each column when not duplicated
+    """
+    #: If all the rows have identical values in test_fields, we assume the data have been duplicated and only return
+    #: the first row
+    if group_df.duplicated(subset=test_fields, keep=False).all():
+        return group_df.iloc[0, group_df.columns.get_indexer(sum_fields)]
+
+    #: Otherwise, sum them by column.
+    return group_df[sum_fields].sum()
 
 
 def _group_common_field_ops(fields_map):
+    """Transpose a dict of {field: op} to {op: [fields]}
+
+    Args:
+        fields_map (dict): Each field and it's operation in {field:op}
+
+    Returns:
+        dict: Dict of ops and the fields they apply to in {op: [fields]}
+    """
 
     ops_and_fields = defaultdict(list)
-    for field, op in fields_map.items():
-        ops_and_fields[op.casefold()].append(field)
+    for field, operation in fields_map.items():
+        ops_and_fields[operation.casefold()].append(field)
 
     return ops_and_fields
-
-
-
 
 
 def _combine_geometries_and_attributes(geometries_df, attributes_df, dissolve_field):
@@ -123,14 +176,14 @@ def _extract_duplicates_and_uniques(dataframe, dissolve_field):
     return duplicates, uniques
 
 
-def dissolve_duplicates_by_dataframe(dataframe, dissolve_field, fields_map):
+def dissolve_duplicates_by_dataframe(dataframe, dissolve_field, fields_map, duplicate_test_fields):
     duplicates, uniques = _extract_duplicates_and_uniques(dataframe, dissolve_field)
 
     dissolved_geometries = _dissolve_geometries(duplicates, dissolve_field)
-    dissolved_attributes = _dissolve_attributes(duplicates, dissolve_field, fields_map)
+    dissolved_attributes = _dissolve_attributes(duplicates, dissolve_field, fields_map, duplicate_test_fields)
 
     dissolved_combined = _combine_geometries_and_attributes(dissolved_geometries, dissolved_attributes, dissolve_field)
 
-    all_combined = _recombine_dissolved_with_all(uniques, dissolved_combined)
+    all_combined = _recombine_dissolved_with_all(dissolved_combined, uniques)
 
     return all_combined
--- tests/test_dissolve.py
+++ tests/test_dissolve.py
@@ -2,7 +2,6 @@ import pandas as pd
 import pytest
 from pandas import testing as tm
 
-import housing_unit_inventory
 from housing_unit_inventory import dissolve
 
 
@@ -136,3 +135,467 @@ class TestExtractAndCombine:
         })
 
         tm.assert_frame_equal(combined_df, test_df)
+
+
+class TestAttributeDissolveHelpers:
+
+    def test_group_common_field_ops_combines_ops(self):
+        field_map = {
+            'foo': 'sum',
+            'bar': 'sum',
+        }
+
+        ops_and_fields = dissolve._group_common_field_ops(field_map)
+
+        test_map = {
+            'sum': ['foo', 'bar'],
+        }
+
+        assert ops_and_fields == test_map
+
+    def test_group_common_field_ops_still_retuns_lists_for_single_field_ops(self):
+        field_map = {
+            'foo': 'sum',
+            'bar': 'count',
+        }
+
+        ops_and_fields = dissolve._group_common_field_ops(field_map)
+
+        test_map = {
+            'sum': ['foo'],
+            'count': ['bar'],
+        }
+
+        assert ops_and_fields == test_map
+
+    def test_smart_groupby_sum_sums_rows_with_all_different_values(self):
+        dup_parcels_df = pd.DataFrame({
+            'PARCEL_ID': ['1', '1', '2'],
+            'BLDG_SQFT': [100, 200, 200],
+            'TOTAL_MKT_VALUE': [50, 10, 40],
+            'LAND_MKT_VALUE': [10, 20, 15],
+            'PARCEL_ACRES': [.5, .1, .1],
+        })
+
+        sum_fields = ['BLDG_SQFT', 'TOTAL_MKT_VALUE', 'LAND_MKT_VALUE']
+        test_fields = sum_fields + ['PARCEL_ACRES']
+
+        summed_df = dup_parcels_df.groupby('PARCEL_ID').agg(dissolve._smart_groupby_sum, sum_fields,
+                                                            test_fields)[sum_fields]
+
+        test_df = pd.DataFrame({
+            'BLDG_SQFT': [300., 200.],
+            'TOTAL_MKT_VALUE': [60., 40.],
+            'LAND_MKT_VALUE': [30., 15.],
+        })
+        test_df.index = ['1', '2']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(summed_df, test_df)
+
+    def test_smart_groupby_sum_sums_rows_with_one_different_value(self):
+        dup_parcels_df = pd.DataFrame({
+            'PARCEL_ID': ['1', '1', '2'],
+            'BLDG_SQFT': [200, 200, 200],
+            'TOTAL_MKT_VALUE': [10, 10, 40],
+            'LAND_MKT_VALUE': [21, 20, 15],
+            'PARCEL_ACRES': [.5, .5, .1],
+        })
+
+        sum_fields = ['BLDG_SQFT', 'TOTAL_MKT_VALUE', 'LAND_MKT_VALUE']
+        test_fields = sum_fields + ['PARCEL_ACRES']
+
+        summed_df = dup_parcels_df.groupby('PARCEL_ID').agg(dissolve._smart_groupby_sum, sum_fields,
+                                                            test_fields)[sum_fields]
+
+        test_df = pd.DataFrame({
+            'BLDG_SQFT': [400., 200.],
+            'TOTAL_MKT_VALUE': [20., 40.],
+            'LAND_MKT_VALUE': [41., 15.],
+        })
+        test_df.index = ['1', '2']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(summed_df, test_df)
+
+    def test_smart_groupby_sum_sums_rows_with_one_different_value_in_a_non_sum_field(self):
+        dup_parcels_df = pd.DataFrame({
+            'PARCEL_ID': ['1', '1', '2'],
+            'BLDG_SQFT': [200, 200, 200],
+            'TOTAL_MKT_VALUE': [10, 10, 40],
+            'LAND_MKT_VALUE': [20, 20, 15],
+            'PARCEL_ACRES': [.5, .6, .1],
+        })
+
+        sum_fields = ['BLDG_SQFT', 'TOTAL_MKT_VALUE', 'LAND_MKT_VALUE']
+        test_fields = sum_fields + ['PARCEL_ACRES']
+
+        summed_df = dup_parcels_df.groupby('PARCEL_ID').agg(dissolve._smart_groupby_sum, sum_fields,
+                                                            test_fields)[sum_fields]
+
+        test_df = pd.DataFrame({
+            'BLDG_SQFT': [400., 200.],
+            'TOTAL_MKT_VALUE': [20., 40.],
+            'LAND_MKT_VALUE': [40., 15.],
+        })
+        test_df.index = ['1', '2']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(summed_df, test_df)
+
+    def test_smart_groupby_sum_doesnt_sum_rows_with_same_values(self):
+        dup_parcels_df = pd.DataFrame({
+            'PARCEL_ID': ['1', '1', '2'],
+            'BLDG_SQFT': [100, 100, 200],
+            'TOTAL_MKT_VALUE': [50, 50, 40],
+            'LAND_MKT_VALUE': [10, 10, 15],
+            'PARCEL_ACRES': [.5, .5, .1],
+        })
+
+        sum_fields = ['BLDG_SQFT', 'TOTAL_MKT_VALUE', 'LAND_MKT_VALUE']
+        test_fields = sum_fields + ['PARCEL_ACRES']
+
+        summed_df = dup_parcels_df.groupby('PARCEL_ID').agg(dissolve._smart_groupby_sum, sum_fields,
+                                                            test_fields)[sum_fields]
+
+        test_df = pd.DataFrame({
+            'BLDG_SQFT': [100., 200.],
+            'TOTAL_MKT_VALUE': [50., 40.],
+            'LAND_MKT_VALUE': [10., 15.],
+        })
+        test_df.index = ['1', '2']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(summed_df, test_df)
+
+    def test_smart_groupby_sum_passes_with_no_duplicate_rows(self):
+        dup_parcels_df = pd.DataFrame({
+            'PARCEL_ID': ['1', '2'],
+            'BLDG_SQFT': [100, 200],
+            'TOTAL_MKT_VALUE': [50, 40],
+            'LAND_MKT_VALUE': [10, 15],
+            'PARCEL_ACRES': [.5, .1],
+        })
+
+        sum_fields = ['BLDG_SQFT', 'TOTAL_MKT_VALUE', 'LAND_MKT_VALUE']
+        test_fields = sum_fields + ['PARCEL_ACRES']
+
+        summed_df = dup_parcels_df.groupby('PARCEL_ID').agg(dissolve._smart_groupby_sum, sum_fields,
+                                                            test_fields)[sum_fields]
+
+        test_df = pd.DataFrame({
+            'BLDG_SQFT': [100., 200.],
+            'TOTAL_MKT_VALUE': [50., 40.],
+            'LAND_MKT_VALUE': [10., 15.],
+        })
+        test_df.index = ['1', '2']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(summed_df, test_df)
+
+
+class TestAttributeDissolve:
+
+    def test_dissolve_attributes_count(self):
+
+        #: duplicates_df, dissolve_field, fields_map, sum_duplicate_test_fields
+
+        dups = pd.DataFrame({
+            'PARCEL_ID': ['1', '1'],
+            'OWNER': ['foo', 'foo'],
+        })
+
+        fields_map = {'OWNER': 'count'}
+
+        dissolved_df = dissolve._dissolve_attributes(dups, 'PARCEL_ID', fields_map, [])
+
+        test_df = pd.DataFrame({'OWNER': [2]})
+        test_df.index = ['1']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(dissolved_df, test_df)
+
+    def test_dissolve_attributes_first_int(self):
+
+        dups = pd.DataFrame({
+            'PARCEL_ID': ['1', '1'],
+            'OWNER': [1, 2],
+        })
+
+        fields_map = {'OWNER': 'first'}
+
+        dissolved_df = dissolve._dissolve_attributes(dups, 'PARCEL_ID', fields_map, [])
+
+        test_df = pd.DataFrame({'OWNER': [1]})
+        test_df.index = ['1']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(dissolved_df, test_df)
+
+    def test_dissolve_attributes_first_str(self):
+
+        dups = pd.DataFrame({
+            'PARCEL_ID': ['1', '1'],
+            'OWNER': ['foo', 'bar'],
+        })
+
+        fields_map = {'OWNER': 'first'}
+
+        dissolved_df = dissolve._dissolve_attributes(dups, 'PARCEL_ID', fields_map, [])
+
+        test_df = pd.DataFrame({'OWNER': ['foo']})
+        test_df.index = ['1']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(dissolved_df, test_df)
+
+    def test_dissolve_attributes_max(self):
+
+        dups = pd.DataFrame({
+            'PARCEL_ID': ['1', '1'],
+            'OWNER': [1, 15],
+        })
+
+        fields_map = {'OWNER': 'max'}
+
+        dissolved_df = dissolve._dissolve_attributes(dups, 'PARCEL_ID', fields_map, [])
+
+        test_df = pd.DataFrame({'OWNER': [15]})
+        test_df.index = ['1']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(dissolved_df, test_df)
+
+    def test_dissolve_attributes_sum_all_fields_duplicated_returns_first(self):
+
+        dups = pd.DataFrame({
+            'PARCEL_ID': ['1', '1'],
+            'TEST': [100, 100],
+            'PARCEL_ACRES': [1, 1],
+        })
+
+        fields_map = {'TEST': 'sum'}
+
+        dissolved_df = dissolve._dissolve_attributes(dups, 'PARCEL_ID', fields_map, ['TEST', 'PARCEL_ACRES'])
+
+        test_df = pd.DataFrame({'TEST': [100]})
+        test_df.index = ['1']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(dissolved_df, test_df)
+
+    def test_dissolve_attributes_sum_mixed_duplicate_unique_fields_returns_sum(self):
+
+        dups = pd.DataFrame({
+            'PARCEL_ID': ['1', '1'],
+            'TEST': [100, 100],
+            'PARCEL_ACRES': [1, 110],
+        })
+
+        fields_map = {'TEST': 'sum'}
+
+        dissolved_df = dissolve._dissolve_attributes(dups, 'PARCEL_ID', fields_map, ['TEST', 'PARCEL_ACRES'])
+
+        test_df = pd.DataFrame({'TEST': [200.]})
+        test_df.index = ['1']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(dissolved_df, test_df)
+
+    def test_dissolve_attributes_sum_all_unique_fields_returns_sum(self):
+
+        dups = pd.DataFrame({
+            'PARCEL_ID': ['1', '1'],
+            'TEST': [100, 200],
+            'PARCEL_ACRES': [1, 110],
+        })
+
+        fields_map = {'TEST': 'sum'}
+
+        dissolved_df = dissolve._dissolve_attributes(dups, 'PARCEL_ID', fields_map, ['TEST', 'PARCEL_ACRES'])
+
+        test_df = pd.DataFrame({'TEST': [300.]})
+        test_df.index = ['1']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(dissolved_df, test_df)
+
+    def test_dissolve_attributes_max(self):
+
+        dups = pd.DataFrame({
+            'PARCEL_ID': ['1', '1'],
+            'TEST': [100, 200],
+        })
+
+        fields_map = {'TEST': 'max'}
+
+        dissolved_df = dissolve._dissolve_attributes(dups, 'PARCEL_ID', fields_map, [])
+
+        test_df = pd.DataFrame({'TEST': [200]})
+        test_df.index = ['1']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(dissolved_df, test_df)
+
+    def test_dissolve_attributes_two_fields_same_op(self):
+
+        dups = pd.DataFrame({
+            'PARCEL_ID': ['1', '1'],
+            'TEST': [100, 200],
+            'TEST2': [300, 400],
+        })
+
+        fields_map = {'TEST': 'max', 'TEST2': 'max'}
+
+        dissolved_df = dissolve._dissolve_attributes(dups, 'PARCEL_ID', fields_map, [])
+
+        test_df = pd.DataFrame({
+            'TEST': [200],
+            'TEST2': [400],
+        })
+        test_df.index = ['1']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(dissolved_df, test_df)
+
+    def test_dissolve_attributes_two_fields_different_op(self):
+
+        dups = pd.DataFrame({
+            'PARCEL_ID': ['1', '1'],
+            'TEST': [100, 200],
+            'TEST2': [300, 400],
+        })
+
+        fields_map = {'TEST': 'max', 'TEST2': 'first'}
+
+        dissolved_df = dissolve._dissolve_attributes(dups, 'PARCEL_ID', fields_map, [])
+
+        test_df = pd.DataFrame({
+            'TEST': [200],
+            'TEST2': [300],
+        })
+        test_df.index = ['1']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(dissolved_df, test_df)
+
+    def test_dissolve_attributes_two_groups_two_fields_same_op(self):
+
+        dups = pd.DataFrame({
+            'PARCEL_ID': ['1', '1', '2', '2'],
+            'TEST': [100, 200, 300, 400],
+            'TEST2': [500, 600, 700, 800],
+        })
+
+        fields_map = {'TEST': 'max', 'TEST2': 'max'}
+
+        dissolved_df = dissolve._dissolve_attributes(dups, 'PARCEL_ID', fields_map, [])
+
+        test_df = pd.DataFrame({
+            'TEST': [200, 400],
+            'TEST2': [600, 800],
+        })
+        test_df.index = ['1', '2']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(dissolved_df, test_df)
+
+    def test_dissolve_attributes_two_groups_two_fields_two_ops(self):
+
+        dups = pd.DataFrame({
+            'PARCEL_ID': ['1', '1', '2', '2'],
+            'TEST': [100, 200, 300, 400],
+            'TEST2': [500, 600, 700, 800],
+        })
+
+        fields_map = {'TEST': 'max', 'TEST2': 'first'}
+
+        dissolved_df = dissolve._dissolve_attributes(dups, 'PARCEL_ID', fields_map, [])
+
+        test_df = pd.DataFrame({
+            'TEST': [200, 400],
+            'TEST2': [500, 700],
+        })
+        test_df.index = ['1', '2']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(dissolved_df, test_df)
+
+    def test_dissolve_attributes_warns_unknown_op(self):
+
+        dups = pd.DataFrame({
+            'PARCEL_ID': ['1', '1'],
+            'TEST': [100, 200],
+        })
+
+        fields_map = {'TEST': 'foo'}
+
+        with pytest.warns(UserWarning) as warning:
+            dissolved_df = dissolve._dissolve_attributes(dups, 'PARCEL_ID', fields_map, [])
+
+        assert warning[0].message.args[0] == 'Attribute operation "foo" not supported, skipping...'
+
+    def test_dissolve_attributes_warns_unknown_op_does_next_op(self):
+
+        dups = pd.DataFrame({
+            'PARCEL_ID': ['1', '1'],
+            'TEST': [100, 200],
+            'TEST2': [300, 400],
+        })
+
+        fields_map = {'TEST': 'foo', 'TEST2': 'first'}
+
+        with pytest.warns(UserWarning) as warning:
+            dissolved_df = dissolve._dissolve_attributes(dups, 'PARCEL_ID', fields_map, [])
+
+        test_df = pd.DataFrame({
+            'TEST2': [300],
+        })
+        test_df.index = ['1']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(dissolved_df, test_df)
+        assert warning[0].message.args[0] == 'Attribute operation "foo" not supported, skipping...'
+
+    def test_dissolve_attributes_handles_dissolve_field_count(self):
+
+        dups = pd.DataFrame({
+            'PARCEL_ID': ['1', '1'],
+            'TEST': [100, 200],
+        })
+
+        fields_map = {'PARCEL_ID': 'count', 'TEST': 'max'}
+
+        dissolved_df = dissolve._dissolve_attributes(dups, 'PARCEL_ID', fields_map, [])
+
+        test_df = pd.DataFrame({
+            'PARCEL_ID_count': [2],
+            'TEST': [200],
+        })
+        test_df.index = ['1']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(dissolved_df, test_df)
+
+    def test_dissolve_attributes_warns_dissolve_field_not_count_op(self):
+
+        dups = pd.DataFrame({
+            'PARCEL_ID': ['1', '1'],
+            'TEST': [100, 200],
+        })
+
+        fields_map = {'PARCEL_ID': 'max', 'TEST': 'max'}
+
+        with pytest.warns(UserWarning) as warning:
+            dissolved_df = dissolve._dissolve_attributes(dups, 'PARCEL_ID', fields_map, [])
+
+        test_df = pd.DataFrame({
+            'PARCEL_ID_max': ['1'],
+            'TEST': [200],
+        })
+        test_df.index = ['1']
+        test_df.index.name = 'PARCEL_ID'
+
+        tm.assert_frame_equal(dissolved_df, test_df)
+        assert warning[0].message.args[
+            0] == 'Dissolve field "PARCEL_ID" should only use "count" operation; result likely nonsensical'
